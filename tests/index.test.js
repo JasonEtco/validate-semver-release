d36956b5641825d0112c8526b9bda8fc8d814678
Jason Etcovitch <jasonetco@github.com>
2019-02-15 00:31:15 -0500

    Write some tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,55 @@
+const path = require('path')
 const runAction = require('..')
+const { Toolkit } = require('actions-toolkit')
+
+function mockToolkit (event) {
+  // Load the relevant JSON file
+  process.env.GITHUB_EVENT_PATH = path.join(
+    __dirname,
+    'fixtures',
+    `${event}.json`
+  )
+
+  // Silence warning
+  Toolkit.prototype.warnForMissingEnvVars = jest.fn()
+
+  // Mock the toolkit exit functions
+  const tools = new Toolkit()
+  tools.exit.neutral = jest.fn()
+  tools.exit.failure = jest.fn()
+  tools.exit.success = jest.fn()
+  return tools
+}
 
 describe('validate-semver-release', () => {
-  it('does the do', () => {
-    expect(1 + 1).toBe(2)
+  beforeEach(() => {
+    Object.assign(process.env, {
+      GITHUB_EVENT: 'release',
+      GITHUB_WORKSPACE: path.join(__dirname, 'fixtures', 'workspace')
+    })
+  })
+
+  it('exits neutral for draft releases', () => {
+    const tools = mockToolkit('release-draft')
+    runAction(tools)
+    expect(tools.exit.neutral).toHaveBeenCalled()
+  })
+
+  it('exits failure for invalid tag', () => {
+    const tools = mockToolkit('release-invalid-tag')
+    runAction(tools)
+    expect(tools.exit.failure).toHaveBeenCalled()
+  })
+
+  it('exits failure for a tag/version mismatch', () => {
+    const tools = mockToolkit('release-wrong-version')
+    runAction(tools)
+    expect(tools.exit.failure).toHaveBeenCalled()
+  })
+
+  it('exits successfully with no issues', () => {
+    const tools = mockToolkit('release')
+    runAction(tools)
+    expect(tools.exit.success).toHaveBeenCalled()
   })
 })
