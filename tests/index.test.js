0d6239b3386ff6103f6fede1d47c9050925fbb55
Jason Etcovitch <jasonetco@github.com>
2019-03-30 14:13:28 -0400

    Update tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const runAction = require('..')
 const { Toolkit } = require('actions-toolkit')
 
 function mockToolkit (event, workspace = 'workspace') {
@@ -28,59 +27,64 @@ function mockToolkit (event, workspace = 'workspace') {
 }
 
 describe('validate-semver-release', () => {
+  let runAction
+
   beforeEach(() => {
+    Toolkit.run = jest.fn(fn => { runAction = fn })
+    require('../entrypoint')
+
     Object.assign(process.env, {
       GITHUB_EVENT: 'release',
       GITHUB_WORKSPACE: path.join(__dirname, 'fixtures', 'workspace')
     })
   })
 
-  it('exits neutral for draft releases', () => {
+  it('exits neutral for draft releases', async () => {
     const tools = mockToolkit('release-draft')
-    runAction(tools)
+    await runAction(tools)
     expect(tools.exit.neutral).toHaveBeenCalled()
     expect(tools.exit.neutral.mock.calls).toMatchSnapshot()
   })
 
-  it('exits failure for invalid tag', () => {
+  it('exits failure for invalid tag', async () => {
     const tools = mockToolkit('release-invalid-tag')
-    runAction(tools)
+    await runAction(tools)
     expect(tools.exit.failure).toHaveBeenCalled()
     expect(tools.exit.failure.mock.calls).toMatchSnapshot()
   })
 
-  it('exits failure for a tag/version mismatch', () => {
+  it('exits failure for a tag/version mismatch', async () => {
     const tools = mockToolkit('release-wrong-version')
-    runAction(tools)
+    await runAction(tools)
     expect(tools.exit.failure).toHaveBeenCalled()
     expect(tools.exit.failure.mock.calls).toMatchSnapshot()
   })
 
-  it('exits successfully with no issues', () => {
+  it('exits successfully with no issues', async () => {
     const tools = mockToolkit('release')
-    runAction(tools)
+    await runAction(tools)
     expect(tools.exit.success).toHaveBeenCalled()
     expect(tools.exit.success.mock.calls).toMatchSnapshot()
   })
 
-  describe('prereleases', () => {
-    it('exits failure with no semver prelease on the tag', () => {
+  describe('prereleases', async () => {
+    it('exits failure with no semver prelease on the tag', async () => {
       const tools = mockToolkit('release-prerelease-not-tag')
-      runAction(tools)
+      await runAction(tools)
       expect(tools.exit.failure).toHaveBeenCalled()
       expect(tools.exit.failure.mock.calls).toMatchSnapshot()
     })
 
-    it('exits failure with an invalid prerelease tag', () => {
+    it('exits failure with an invalid prerelease tag', async () => {
       const tools = mockToolkit('release-prerelease-invalid-tag', 'workspace-invalid-prerelease')
-      runAction(tools)
+      await runAction(tools)
       expect(tools.exit.failure).toHaveBeenCalled()
       expect(tools.exit.failure.mock.calls).toMatchSnapshot()
     })
 
-    it('writes to a file with a valid tag and version', () => {
+    it('writes to a file with a valid tag and version', async () => {
       const tools = mockToolkit('release-prerelease', 'workspace-prerelease')
-      runAction(tools)
+      await runAction(tools)
       expect(tools.exit.success).toHaveBeenCalled()
       expect(tools.exit.success.mock.calls).toMatchSnapshot()
       const file = tools.getFile('release-workflow-tag')
